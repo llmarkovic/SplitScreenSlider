2a5b32f4d6de3aa7b44fdd234888719c895f4cc2
Luka Markovic <markovicluka29@gmail.com>
2020-07-10 20:21:01 +0200

    Initial version; just JS left to finish

---
+++ run.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let wrapper = document.getElementById('wrapper');
+    let topLayer = wrapper.querySelector('.top');
+    let handle = wrapper.querySelector('.handle');
+    let skew = 0;
+    let delta = 0;
+});
+
